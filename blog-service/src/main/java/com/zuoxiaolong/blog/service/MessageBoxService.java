35db9fc02e3a81ebcefa3b232718f7d22746acd5
voyagezhang <voyagezhang@126.com>
2016-05-20 10:06:12 +0800

    添加博客信息箱功能

--- blog-service/src/main/java/com/zuoxiaolong/blog/service/MessageBoxService.java
+++ blog-service/src/main/java/com/zuoxiaolong/blog/service/MessageBoxService.java
@@ -25,7 +25,6 @@ import java.util.List;
         * @date 2016/5/16
         * @since 1.0.0
         */
-@Service
 public interface MessageBoxService {
     MessageBox selectByPrimaryKey(Integer id);
 
