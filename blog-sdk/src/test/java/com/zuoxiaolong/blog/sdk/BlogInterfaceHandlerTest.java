315798fbffb1b11e9d0912df8cf21e95d89537f7
Boren You <520jiuweidaji@163.com>
2016-05-26 22:44:34 +0800

    添加说明文件

--- blog-sdk/src/test/java/com/zuoxiaolong/blog/sdk/BlogInterfaceHandlerTest.java
+++ blog-sdk/src/test/java/com/zuoxiaolong/blog/sdk/BlogInterfaceHandlerTest.java
@@ -13,31 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.zuoxiaolong.blog.sdk.util.http.constants;
+package com.zuoxiaolong.blog.sdk;
+
+import com.zuoxiaolong.blog.model.dto.cache.ArticleRankResponseDto;
+
+import java.util.List;
 
 /**
  * @author Boren You
- * @dateTime 2016/5/25 22:44
+ * @dateTime 2016/5/26 22:18
  * @since 1.0.0
  */
-public interface Constants {
-    String DEFAULT_CHARSET = "UTF-8";
-
-    String PARAMETER_SEPARATOR = "&";
-
-    String EQUAL_SIGN = "=";
-
-    String QUESTION_MARK = "?";
-
-    Integer DEFAULT_CONNECT_TIMEOUT = 5000;
-
-    Integer DEFAULT_READ_TIMEOUT = 5000;
-
-    String DEFAULT_CONTENT_TYPE = "application/x-www-form-urlencoded";
-
-    String METHOD_POST = "POST";
-
-    String METHOD_GET = "GET";
-
-    Integer HTTP_CODE_200 = 200;
+public class BlogInterfaceHandlerTest {
+    public static void main(String[] args) throws Exception {
+        List<ArticleRankResponseDto> list = BlogInterfaceHandler.getArticleRank();
+    }
 }
