b48e352cb558f097d067a1893ce863f2b0db235a
Boren You <520jiuweidaji@163.com>
2016-05-26 20:35:42 +0800

    让 获取文章排名数据的接口 去掉限制只允许get提交

--- blog-cache/src/main/java/com/zuoxiaolong/blog/cache/controller/ArticleRankController.java
+++ blog-cache/src/main/java/com/zuoxiaolong/blog/cache/controller/ArticleRankController.java
@@ -26,7 +26,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
