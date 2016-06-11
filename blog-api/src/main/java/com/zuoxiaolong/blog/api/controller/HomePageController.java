1809e980c96dafec86c4c366944353c47574de27
“iCodie <codingstar@qq.com>
2016-06-11 11:01:09 +0800

    去掉多余的空行

--- blog-api/src/main/java/com/zuoxiaolong/blog/api/controller/HomePageController.java
+++ blog-api/src/main/java/com/zuoxiaolong/blog/api/controller/HomePageController.java
@@ -13,11 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.zuoxiaolong.blog.api.controller;
 
-
-import com.zuoxiaolong.blog.common.spring.BaseController;
 import com.zuoxiaolong.blog.model.persistent.UserArticle;
 import com.zuoxiaolong.blog.service.UserArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +22,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
@@ -43,11 +39,6 @@ public class HomePageController extends ApiBaseController {
     @Autowired
     private UserArticleService userArticleService;
 
-    @RequestMapping(value = {"/TopThreeUserArticles"}, method = {RequestMethod.GET, RequestMethod.POST})
-    public List<Map<String, UserArticle>> topThreeUserArticles(String categoryName) {
-        return userArticleService.getTopThreeUserArticles(categoryName);
-    }
-
     @RequestMapping(value = "/Articles", method = RequestMethod.POST)
     public List<UserArticle> getArticles(@RequestParam("categoryId") int categoryId,
                                          @RequestParam(required = false, defaultValue = "1") int pageNum,
@@ -57,6 +48,13 @@ public class HomePageController extends ApiBaseController {
         params.put("startRow", (pageNum - 1) * pageSize);
         params.put("pageSize", pageSize);
         List<UserArticle> s = userArticleService.getArticles(params);
+
         return s;
     }
+
+    @RequestMapping(value = {"/TopThreeUserArticles"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public List<Map<String, UserArticle>> topThreeUserArticles(String categoryName) {
+        return userArticleService.getTopThreeUserArticles(categoryName);
+    }
+
 }
