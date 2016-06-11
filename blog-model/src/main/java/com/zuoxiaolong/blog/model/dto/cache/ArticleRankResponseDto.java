756eae0e4bbedff8753519225b0c6fb60b3630cc
“iCodie <codingstar@qq.com>
2016-06-11 11:06:06 +0800

    去掉多余的空行

--- blog-model/src/main/java/com/zuoxiaolong/blog/model/dto/cache/ArticleRankResponseDto.java
+++ blog-model/src/main/java/com/zuoxiaolong/blog/model/dto/cache/ArticleRankResponseDto.java
@@ -25,7 +25,6 @@ import java.util.List;
  */
 public class ArticleRankResponseDto {
     private String actionType;
-
     private List<ArticleRankResponseDataResult> dataResult;
 
     public String getActionType() {
