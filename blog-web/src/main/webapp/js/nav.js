ca7b7fab8b50104c33c374588511913d6644cadb
“iCodie <codingstar@qq.com>
2016-06-12 15:41:20 +0800

    删除用console输出排名文章数据的测试

--- blog-web/src/main/webapp/js/nav.js
+++ blog-web/src/main/webapp/js/nav.js
@@ -7,13 +7,13 @@ $(function () {
     //打开网站时加载技术类文章并显示出来
     var url = "/HomePage/TopThreeUserArticles";
     var categoryName = "技术";
-    getDataAndParse(url, {categoryName:categoryName});
+    getDataAndParse(url, {categoryName: categoryName});
     //根据用户对菜单的选择显示相应的最多评论、最多推荐、最多阅读的文章
     $(".blog-nav").on("click", "a", function (event) {
         $(".blog-nav a").removeClass("active");
         $(this).addClass("active");
         categoryName = $(this).text();
-        getDataAndParse(url, {categoryName:categoryName});
+        getDataAndParse(url, {categoryName: categoryName});
     });
 })
 
@@ -31,7 +31,7 @@ function getDataAndParse(url, data) {
         type: "POST",
         cache: false,
         success: function (data, status) {
-        console.log(data);
+
         },
         error: function () {
 
