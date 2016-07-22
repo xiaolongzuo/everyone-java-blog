1e6e4b2e1e5d0c338e81a45d054abcfad9fe1c88
Xiaolong Zuo <150349407@qq.com>
2016-07-22 22:17:27 +0800

    添加数据初始化SQL.

---
+++ blog-dao/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `java_blog`.`article_category` (id,category_name) VALUES (1,'技术');
+INSERT INTO `java_blog`.`article_category` (id,category_name) VALUES (2,'职场');
+INSERT INTO `java_blog`.`article_category` (id,category_name) VALUES (3,'人生');
